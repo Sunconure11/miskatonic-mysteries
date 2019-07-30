767e372aa3d080ebce4d29b6c98124dc80ddbc2a
MoriyaShiine <moriyashiine@gmail.com>
2019-07-30 16:11:28 -0500

    oof

--- src/main/java/moriyashiine/acme/registry/ModObjects.java
+++ src/main/java/moriyashiine/acme/registry/ModObjects.java
@@ -12,6 +12,7 @@ public class ModObjects
 	public static Block prismarine_cthulhu_mural = Util.create(new Block(Material.ROCK), SoundType.STONE, 1.5f, 30, "pickaxe", 0, "prismarine_cthulhu_mural");
 	
 	public static Block stone_hastur_mural = Util.create(new Block(Material.ROCK), SoundType.STONE, 1.5f, 30, "pickaxe", 0, "stone_hastur_mural");
+	public static Block moss_stone_hastur_mural = Util.create(new Block(Material.ROCK), SoundType.STONE, 1.5f, 30, "pickaxe", 0, "moss_stone_hastur_mural");
 	public static Block terracotta_hastur_mural = Util.create(new Block(Material.ROCK), SoundType.STONE, 1.25f, 21, "pickaxe", 0, "terracotta_hastur_mural");
 	public static Block yellow_terracotta_hastur_mural = Util.create(new Block(Material.ROCK), SoundType.STONE, 1.25f, 21, "pickaxe", 0, "yellow_terracotta_hastur_mural");
 	
