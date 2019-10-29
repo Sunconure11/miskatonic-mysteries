407fe387275a02d866cb1d76f6183a75feacddf2
Aelpecyem <aelpecyem@gmail.com>
2019-10-29 21:52:16 +0100

    aaa

---
+++ src/main/java/com/miskatonicmysteries/common/misc/rites/RiteBlackMass.java
@@ -0,0 +1,76 @@
+package com.miskatonicmysteries.common.misc.rites;
+
+import com.miskatonicmysteries.MiskatonicMysteries;
+import com.miskatonicmysteries.common.block.tile.TileEntityOctagram;
+import com.miskatonicmysteries.common.capability.blessing.blessings.Blessing;
+import com.miskatonicmysteries.common.entity.EntityDarkYoung;
+import com.miskatonicmysteries.registry.ModObjects;
+import com.miskatonicmysteries.util.WorldGenUtil;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleSmokeLarge;
+import net.minecraft.client.particle.ParticleSpell;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.World;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class RiteBlackMass extends OctagramRite {
+    public RiteBlackMass() {
+        super(new ResourceLocation(MiskatonicMysteries.MODID, "eldritch_trap"), 100, 50, 200, EnumType.FOCUSED, Blessing.SHUB, Blessing.SHUB, Ingredient.fromItem(Items.MUTTON), Ingredient.fromItem(Items.MUTTON), Ingredient.fromItem(Items.ENDER_PEARL), Ingredient.fromItem(Items.ENDER_PEARL), Ingredient.fromItem(ModObjects.infested_wheat), Ingredient.fromItem(ModObjects.infested_wheat), Ingredient.fromStacks(new ItemStack(Blocks.WOOL, 1, EnumDyeColor.BLACK.getDyeDamage())), Ingredient.fromStacks(new ItemStack(Blocks.WOOL, 1, EnumDyeColor.BLACK.getDyeDamage())));
+    }
+
+    @Override
+    public boolean test(TileEntityOctagram octagram) {
+        return octagram.getWorld().getWorldInfo().getDifficulty() != EnumDifficulty.PEACEFUL;
+    }
+
+    @Override
+    public void doRitual(TileEntityOctagram octagram, @Nullable EntityPlayer caster) {
+        if (octagram.getWorld().isRemote)
+            spawnParticles(octagram);
+    }
+
+    @SideOnly(Side.CLIENT)
+    private void spawnParticles(TileEntityOctagram octagram) {
+        Particle p = new ParticleSmokeLarge.Factory().createParticle(0, octagram.getWorld(), octagram.getPos().getX() + 0.5 + (octagram.getWorld().rand.nextGaussian() / 4), octagram.getPos().getY(), octagram.getPos().getZ() + 0.5 + (octagram.getWorld().rand.nextGaussian() / 4), 0, octagram.getWorld().rand.nextFloat() / 10F, 0);
+        p.setRBGColorF(0.05F, 0.5F, 0.4F);
+        Minecraft.getMinecraft().effectRenderer.addEffect(p);
+    }
+
+
+
+    @Override
+    public void effect(TileEntityOctagram octagram, @Nullable EntityPlayer trigger) {
+        World world = octagram.getWorld();
+
+        for (int i = 0; i < 2; i++) {
+            EntityLiving entity = new EntityDarkYoung(world);
+                WorldGenUtil.spawnEntity(entity, world, octagram.getPos().up(), 16, 16);
+        }
+    }
+
+
+    @Override
+    public boolean checkShouldTrigger(TileEntityOctagram octagram, @Nullable EntityPlayer closest) {
+        return !octagram.getWorld().getEntitiesWithinAABB(EntityLivingBase.class, Block.FULL_BLOCK_AABB.grow(1, 0, 1).offset(octagram.getPos()), l -> l instanceof IMob || l instanceof EntityPlayer).isEmpty();
+
+    }
+}
